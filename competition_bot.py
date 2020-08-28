af3c03e37df270aad8195bde41cdd9f86cd89c6c
nevesgmd <gmedeirosneves@gmail.com>
2020-08-28 18:38:14 -0300

    refactor: creating CompetitionBot class

--- competition_bot.py
+++ competition_bot.py
@@ -2,21 +2,28 @@ from discord.ext import commands
 from json import load
 from loguru import logger
 
-client = commands.Bot(command_prefix='')
+
+class CompetitionBot(commands.Cog):
+    def __init__(self, bot):
+        self.__bot = bot
+        self.__competition_name = str()
+        self.__players_points = dict()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.debug('Bot is running!')
+
+    @commands.command(aliases=['comp'])
+    async def competition(self, ctx, *, comp_name):
+        logger.debug('Competition function was called.')
+        self.__competition_name = comp_name
+        await ctx.send(f'-----> **{comp_name}** competition was created! <-----')
 
 
-@client.event
-async def on_ready():
-    logger.debug('Bot is running.')
-
-
-@client.command(aliases=['comp'])
-async def competition(ctx, *, comp_name):
-    logger.debug('Competition function was called.')
-    await ctx.send(f'{comp_name} competition was created!')
-
+discord_bot = commands.Bot(command_prefix='')
+discord_bot.add_cog(CompetitionBot(discord_bot))
 
 with open('.globalresources.json') as global_resources:
     token = load(global_resources)['token']
 
-client.run(token)
+discord_bot.run(token)
