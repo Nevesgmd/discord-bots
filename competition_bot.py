4569314dd94b3f41c5b9b133c514fc3176bbd0dd
nevesgmd <gmedeirosneves@gmail.com>
2020-08-27 23:42:03 -0300

    feat: creating competition command

--- competition_bot.py
+++ competition_bot.py
@@ -2,15 +2,21 @@ from discord.ext import commands
 from json import load
 from loguru import logger
 
-client = commands.Bot(command_prefix=' ')
+client = commands.Bot(command_prefix='')
 
 
 @client.event
 async def on_ready():
     logger.debug('Bot is running.')
 
+
+@client.command(aliases=['comp'])
+async def competition(ctx, *, comp_name):
+    logger.debug('Competition function was called.')
+    await ctx.send(f'{comp_name} competition was created!')
+
+
 with open('.globalresources.json') as global_resources:
     token = load(global_resources)['token']
-    print(token)
 
 client.run(token)
