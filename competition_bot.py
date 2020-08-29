c54f60b5343698b7947fba2e2b1d303dbbce5852
nevesgmd <gmedeirosneves@gmail.com>
2020-08-29 10:40:29 -0300

    feat: adding add_players commmand
    Defined function that add players to an existent competition.

--- competition_bot.py
+++ competition_bot.py
@@ -6,8 +6,7 @@ from loguru import logger
 class CompetitionBot(commands.Cog):
     def __init__(self, bot):
         self.__bot = bot
-        self.__competition_name = str()
-        self.__players_points = dict()
+        self.__competitions = dict()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -16,8 +15,24 @@ class CompetitionBot(commands.Cog):
     @commands.command(aliases=['comp'])
     async def competition(self, ctx, *, comp_name):
         logger.debug('Competition function was called.')
-        self.__competition_name = comp_name
+        self.__competitions[comp_name] = {}
         await ctx.send(f'-----> **{comp_name}** competition was created! <-----')
+
+    @commands.command(aliases=['add'])
+    async def add_players(self, ctx, comp_name, *players):
+        try:
+            for player in players:
+                self.__competitions[comp_name][player] = 0
+                await ctx.send(f"--> **{player}** added to competition **{comp_name}**.")
+                logger.debug(f'{player} added to competition {comp_name}')
+        except KeyError:
+            await ctx.send(f"---!! The **{comp_name}** competition doesn't exist. !!---")
+            logger.debug(f'Trying to add players to an inexistent competition.')
+
+    @commands.command(aliases=['get'])
+    async def get_comp_values(self, ctx):
+        await ctx.send(f'{self.__competitions}')
+        logger.debug(f'{self.__competitions}')
 
 
 discord_bot = commands.Bot(command_prefix='')
