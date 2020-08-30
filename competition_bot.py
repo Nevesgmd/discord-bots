e77e54ddbdb3af0d020dc49aed4da0728cb10d54
nevesgmd <gmedeirosneves@gmail.com>
2020-08-30 20:07:20 -0300

    feat: creating add_points() commmand
    Creating add_poinsts() command and changing some aliases of other commands.

--- competition_bot.py
+++ competition_bot.py
@@ -12,13 +12,13 @@ class CompetitionBot(commands.Cog):
     async def on_ready(self):
         logger.debug('Bot is running!')
 
-    @commands.command(aliases=['comp'])
+    @commands.command(aliases=['comp', 'create'])
     async def competition(self, ctx, *, comp_name):
         logger.debug('Competition function was called.')
         self.__competitions[comp_name] = {}
         await ctx.send(f'-----> **{comp_name}** competition was created! <-----')
 
-    @commands.command(aliases=['add'])
+    @commands.command(aliases=['players'])
     async def add_players(self, ctx, comp_name, *players):
         try:
             for player in players:
@@ -26,8 +26,18 @@ class CompetitionBot(commands.Cog):
                 await ctx.send(f"--> **{player}** added to competition **{comp_name}**.")
                 logger.debug(f'{player} added to competition {comp_name}')
         except KeyError:
-            await ctx.send(f"---!! The **{comp_name}** competition doesn't exist. !!---")
+            await ctx.send(f"!!--- The **{comp_name}** competition doesn't exist. ---!!")
             logger.debug(f'Trying to add players to an inexistent competition.')
+
+    @commands.command(aliases=['add', 'points'])
+    async def add_points(self, ctx, comp, player, points):
+        try:
+            self.__competitions[comp][player] += int(points)
+            logger.debug(f'{points} points added to {player}!')
+            await ctx.send(f'--> **{points}** points added to {player}!')
+        except ValueError:
+            logger.debug(f'Incorrect points parameter value on add_points()')
+            await ctx.send(f'!!--- **Points** must be numeric ---!!')
 
     @commands.command(aliases=['get'])
     async def get_comp_values(self, ctx):
