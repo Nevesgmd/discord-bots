e91de9ddf9ea472a64f8c967171d6989e72e50f7
nevesgmd <gmedeirosneves@gmail.com>
2020-09-09 23:52:00 -0300

    feat: changing command prefix

--- discord_bot.py
+++ discord_bot.py
@@ -2,7 +2,7 @@ from cogs.competition_cog import CompetitionCog
 from discord.ext import commands
 from json import load
 
-discord_bot = commands.Bot(command_prefix='')
+discord_bot = commands.Bot(command_prefix='!')
 discord_bot.add_cog(CompetitionCog(discord_bot))
 
 with open('.globalresources.json') as global_resources:
